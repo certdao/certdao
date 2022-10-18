7aaa8bc7a88ba2f17c880175517f3205cdcc0a85
0xbeary <0xbeary@protonmail.com>
2022-10-17 21:26:50 -0400

    Add full e2e test

--- contracts/Certdao.sol
+++ contracts/Certdao.sol
@@ -11,6 +11,7 @@ contract CertDao {
     address public owner;
     string public constant CERTDAO_DOMAIN = "www.certdao.net";
     uint32 public constant EXPIRATION_PERIOD = 365 days;
+    uint64 public constant PAY_AMOUNT = 0.05 ether;
 
     enum domainStatus {
         pending,
@@ -26,6 +27,7 @@ contract CertDao {
         address ownerAddress;
         domainStatus status;
         uint256 timestamp;
+        string description;
     }
 
     mapping(address => domainOwnerInfo) contractAddressTodomainOwner;
@@ -43,8 +45,8 @@ contract CertDao {
 
     constructor() {
         owner = msg.sender;
-        domainOwnerInfo memory certDaoOwner = domainOwnerInfo(CERTDAO_DOMAIN, owner, domainStatus.approved, block.timestamp);
-        // Assign the deployment address of the certdao contract as the owner of the certdao domain.
+        domainOwnerInfo memory certDaoOwner = domainOwnerInfo(CERTDAO_DOMAIN, owner, domainStatus.approved, block.timestamp + 36525 days, "CertDao Owner genisis");
+        // Assign the deployment address of the certdao contract as the owner of the certdaeo domain.
         contractAddressTodomainOwner[address(this)] = certDaoOwner;
     }
 
@@ -56,25 +58,58 @@ contract CertDao {
         return contractAddressTodomainOwner[contractAddress].ownerAddress;
     }
 
-    function getDomainStatus(address contractAddress) public view returns (domainStatus) {
-        return contractAddressTodomainOwner[contractAddress].status;
+    function getDomainStatus(address contractAddress) public view returns (string memory) {
+        require(contractAddress != address(0), "Contract address is empty");
+        domainStatus status = contractAddressTodomainOwner[contractAddress].status;
+
+        if (status == domainStatus.pending) {
+            return "pending";
+        }
+        if (status == domainStatus.expired) {
+            return "expired";
+        }
+        if(status == domainStatus.approved) {
+            return "approved";
+        }
+        if(status == domainStatus.rejected) {
+            return "rejected";
+        }
+        if(status == domainStatus.revoked) {
+            return "revoked";
+        }
+        if(status == domainStatus.manualFlag) {
+            return "manualFlag";
+        }
+
+        return "unknown";
+
     }
 
     function getDomainName(address contractAddress) public view returns (string memory) {
+        require(contractAddress != address(0), "Contract address is empty");
         return contractAddressTodomainOwner[contractAddress].domainName;
     }
 
-    function submitForValidation(address contractAddress, string memory domainName) public payable {
-        require(msg.value == 0.05 ether, "Please send 0.05 ether to start the validation process.");
+    function requireNotEmptyDomainAndContract(string memory domainName, address contractAddress) private pure {
+        require(bytes(domainName).length != 0, "Domain name is empty");
+        require(contractAddress != address(0), "Contract address is empty");
+    }
+
+    function submitForValidation(string memory domainName, address contractAddress) public payable {
+
+        console.log("Domain Name: %s, Contract: %s", domainName, contractAddress);
+        requireNotEmptyDomainAndContract(domainName, contractAddress);
+
+        require(msg.value == PAY_AMOUNT, "Please send 0.05 ether to start the validation process.");
 
         require(compareStrings(contractAddressTodomainOwner[contractAddress].domainName, ""), "Domain name already registered in struct.");
 
         // Send 0.05 ether to the owner of the contract
         (bool sent, bytes memory data) = owner.call{value: msg.value}("");
-        require(sent, "Failed to send Ether");
+        require(sent, "Failed to send Ether!");
 
         // Add the domain name to the struct
-        domainOwnerInfo memory domainOwner = domainOwnerInfo(domainName, msg.sender, domainStatus.pending, block.timestamp);
+        domainOwnerInfo memory domainOwner = domainOwnerInfo(domainName, msg.sender, domainStatus.pending, block.timestamp, "");
         contractAddressTodomainOwner[contractAddress] = domainOwner;
 
     }
@@ -82,15 +117,14 @@ contract CertDao {
     function verify(string memory domainName , address contractAddress) view public returns (bool) {
         console.log("Inputs: %s, %s", domainName, contractAddress);
 
-        require(bytes(domainName).length != 0, "Domain name is empty");
-        require(contractAddress != address(0), "Contract address is empty");
+        requireNotEmptyDomainAndContract(domainName, contractAddress);
 
         domainOwnerInfo memory domainOwner = contractAddressTodomainOwner[contractAddress];
 
         if(compareStrings(domainOwner.domainName, domainName) && domainOwner.status == domainStatus.approved) {
             // TODO: Add other conditions (is the domain expired, is the domain approved, etc.)
             if(domainOwner.timestamp + EXPIRATION_PERIOD > block.timestamp) {
-                console.log("Domain name and contract address match");
+                console.log("Contract is verified");
                 return true;
             }
             else {
@@ -99,17 +133,24 @@ contract CertDao {
                 return false;
             }
         } else {
-            console.log("Domain name and contract address do not match");
+            console.log("Domain name and contract address do not match or contract is not approved. Check contract state");
             return false;
         }
 
     }
 
+    function renew() public payable {}
+    function transferOwnership() public payable {}
+    // function manualFlag() public onlyOwner {}
 
-    function renew() public {}
+    function revoke() public onlyOwner {}
+    function approve(address contractAddress) public onlyOwner {
+        require(contractAddress != address(0), "Contract address is empty");
+        contractAddressTodomainOwner[contractAddress].status = domainStatus.approved;
+    }
 
-    function revoke() public {}
 
-    function transferOwnership() public {}
+    // TODO: Is this necessary?
+    // function updateFields() public {}
 
 }
--- test/Certdao.test.js
+++ test/Certdao.test.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
-
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("main certdao contract", function () {
   async function deployTokenFixture() {
@@ -28,8 +28,88 @@ describe("main certdao contract", function () {
 
     it("Should return true if certdao is registered", async function () {
       const { certdao } = await loadFixture(deployTokenFixture);
-      expect(await certdao.verify("www.certdao.org", certdao.address)).to.equal(
+      expect(await certdao.verify("www.certdao.net", certdao.address)).to.equal(
         true
+      );
+    });
+  });
+
+  describe("Test main certdao functionality", function () {
+    it("Should not succeed if no funds sent", async function () {
+      const { certdao } = await loadFixture(deployTokenFixture);
+      await expect(
+        certdao.submitForValidation("www.certdao.net", certdao.address)
+      ).to.be.revertedWith(
+        "Please send 0.05 ether to start the validation process."
+      );
+    });
+
+    it("Should not succeed if contract already registered", async function () {
+      const { certdao, addr1 } = await loadFixture(deployTokenFixture);
+
+      const payableParams = {
+        value: ethers.utils.parseEther("0.05"),
+      };
+
+      await expect(
+        certdao.submitForValidation(
+          "www.certdao.net",
+          certdao.address,
+          payableParams
+        )
+      ).to.be.revertedWith("Domain name already registered in struct.");
+    });
+
+    it("Should not succeed if contract already registered", async function () {
+      const { certdao, addr1 } = await loadFixture(deployTokenFixture);
+
+      const payableParams = {
+        value: ethers.utils.parseEther("0.05"),
+      };
+
+      await expect(
+        certdao.submitForValidation(
+          "www.certdao.net",
+          certdao.address,
+          payableParams
+        )
+      ).to.be.revertedWith("Domain name already registered in struct.");
+    });
+
+    it("Fully validation should succeed.", async function () {
+      const { certdao, addr1, owner } = await loadFixture(deployTokenFixture);
+
+      // Deploy a second contract to test the validation process
+      const { certdao: testContract, addr2 } = await deployTokenFixture();
+
+      const payableParams = {
+        from: addr2.address,
+        value: ethers.utils.parseEther("0.05"),
+      };
+
+      const domainToTest = "www.testsite.com";
+
+      // Submit for validation
+      await certdao
+        .connect(addr2)
+        .submitForValidation(domainToTest, testContract.address, payableParams);
+
+      // Approve the contract
+      await certdao.connect(owner).approve(testContract.address);
+
+      // Check that the domain and contract are registered and approved in the struct
+      expect(await certdao.verify(domainToTest, testContract.address)).to.equal(
+        true
+      );
+
+      // Get the owner of the domain
+      expect(await certdao.getDomainOwner(testContract.address)).to.equal(
+        addr2.address
+      );
+
+      // Check that the status is pending
+      expect(await certdao.getDomainStatus(testContract.address)).to.equal(
+        "approved"
       );
     });
   });
